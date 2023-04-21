67af7dba417e4c4129d877f3f6e851a65433b59f
ThaysonGuedes <thaysonguedes01@gmail.com>
2023-04-21 20:48:21 -0300

    add conversaoDados

---
+++ conversao de dados/tiposDadosConversoes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Conversao de tipos
+
+
+
+    // tipos de operadores
+        /*
+            SOMA = +
+            SUBTR = -
+            MULT = *
+            DIV = /
+            RESTO DE DIV = %
+
+        */
+
+
+    int a = 10;
+    int b = 20;
+
+    float resto = a / b;
+
+    printf("%f", (float)resto);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
