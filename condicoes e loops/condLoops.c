e62993ec9ccd6f1b123202b6726ea138a972d9e5
ThaysonGuedes <thaysonguedes01@gmail.com>
2023-04-22 12:44:53 -0300

    update questions and ++ condicionais/loops

---
+++ condicoes e loops/condLoops.c
@@ -0,0 +1,80 @@
+/* tipos de condicoes
+    //ifs
+        if(){
+        }
+
+        if(){
+        } else{
+        }
+
+        if(){
+        } else if(){
+        }
+
+        if(){
+            if(){
+
+            }
+        }
+
+        if(){
+            if(){
+
+            } else{
+
+            }
+        }
+
+
+        if(){
+
+        } else if(){
+
+        } else {
+            if(){
+
+            } else{
+
+            }
+        }
+
+
+
+    //switch
+        switch(variavel  condicao):
+
+            case cond:
+
+                break;
+
+            default:
+                algo default aqui;
+
+
+
+    // Operador ternario
+        (variavel condicao) ? casoVerdadeiro : CasoFalso;
+
+*/
+
+
+
+
+/*
+    //loops
+        //loop for
+            for(variavel = valor; variavel condicao variavel; variavel incremento/decremento){
+                bloco de comando
+            }
+
+
+
+        //loop while
+            variavel de inicializacao de loop
+            while(variavel acima condicional) {
+                bloco de comando
+
+                controle do loop, decremento/incremento da variavel
+            }
+*/
+
--- questions/lista 1/resolucao.c
+++ questions/lista 1/resolucao.c
@@ -2,32 +2,67 @@
 
 int questao2()
 {
-    int a, b;
-    int a2, b2;
+    /*
+        2) Elabore um algoritmo que receba, por meio do teclado, dois valores,
+        um para a variável “a” e um
+        para a variável “b”. Em seguida, faça os passos que julgar necessário para que ao final,
+        a variável “a” possua o valor que inicialmente estava em “b”
+        e a variável “b” possua o valor que inicialmente estava em “a”.
+        Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+    */
+
+    int a, b, c;
 
     printf("digite valores pra a e b\n\n");
     scanf("%d %d", &a, &b);
 
-    a2 = b;
-    b2 = a;
 
-    printf("valor de a: %d; valor de b: %d\n", a2, b2);
+    /*  se: a = 10; b = 50
+
+        c = a = 10
+        a = b = 50
+        b = c = 10
+
+        entao:
+        a = 50; b = 10
+    */
+    c = a;
+    a = b;
+    b = c;
+
+    printf("valor de a: %d; valor de b: %d\n", a, b);
 }
 
 
 int questao3()
 {
+    /*
+        3) Faça um programa em C para trocar o valor de
+        duas variáveis inteiras sem utilizar nenhuma variável auxiliar
+    */
+
     int val1, val2;
 
     printf("digite valores pra a e b\n\n");
     scanf("%d %d", &val1, &val2);
 
-    printf("\nvalor1:%d; valor2:%d\n", val2, val1);
+    val1 = val1 + val2;
+    val2 = val1 - val2;
+    val1 = val1 - val2;
+
+    printf("\nvalor1:%d; valor2:%d\n", val1, val2);
 }
 
 
 int questao4()
 {
+    /*
+        4) Escreva um programa que leia um valor de despesa de restaurante, o valor da gorjeta (em
+        porcentagem) e o número de pessoas para dividir a conta. Imprima o valor que cada um deve pagar.
+        Assuma que a conta será dividida igualmente.
+
+    */
+
     float DespesaRestaurante;
     float Gorjeta;
     int PessoasConta;
@@ -45,6 +80,13 @@ int questao4()
 
 int questao5()
 {
+    /*
+        5) Uma empresa contrata um encanador a R$ 45,00 por dia. Faça um programa que solicite o
+        número de dias trabalhados pelo encanador e imprima a quantia líquida que deverá ser paga,
+        sabendo que são descontados 8% para imposto de renda.
+
+    */
+
     float GanhaEncanador = 45.00;
     int DiasTrabalhados;
     float Imposto = 0.08;
@@ -65,6 +107,12 @@ int questao5()
 
 int questao6()
 {
+    /*
+        6) Crie um programa em C que permita fazer a conversão cambial entre Reais e Dólares. Considere
+        como taxa de câmbio US$1,0 = R$5,30. Leia um valor em Reais e mostre o correspondente em
+        Dólares.
+    */
+
     float CambioDollarReal = 5.30;
     float Real;
     float ValorFinalDollar;
@@ -87,18 +135,31 @@ int questao7()
         Saída: 1:1:12
     */
 
-    int Segundos, Minuto, Hora;
-    Minuto = 60; //60 segundos === 1 minuto
-    Hora = 3600; // 3600 segundos === 1 hora
+    int Segundos, Minuto, Hora, resto, segundoFinal;
 
-    printf("digite segundos para converter em hora, minuto e segundo");
+    printf("digite segundos para converter em hora, minuto e segundo\n\n");
     scanf("%d", &Segundos);
 
-    Minuto = Minuto / Segundos;
-    Hora = Hora / (Minuto * Segundos);
+    Hora = Segundos / 3600;
+    resto = Segundos % 3600;
+    Minuto = resto / 60;
+    segundoFinal = resto % 60;
 
-    printf("horas:minutos:segundos --->  %d:%d:%d", Hora, Minuto, Segundos);
+    printf("\nhoras:minutos:segundos --->  %d:%d:%d\n", Hora, Minuto, segundoFinal);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -118,27 +179,49 @@ int main(void)
     printf("\ndigite 2, 3, 4, 5, 6 ou 7 para exibir cada questao\n");
     scanf("%d", &num);
 
-    if(num == 2) {
-        printf("questao 2\n\n");
-        questao2();
-    } else if(num == 3) {
-        printf("\n\nquestao 3\n\n");
-        questao3();
-    } else if(num == 4) {
-        printf("\n\nquestao 4\n\n");
-        questao4();
-    } else if(num == 5) {
-        printf("\n\nquestao 5\n\n");
-        questao5();
-    } else if(num == 6) {
-        printf("\n\nquestao 6\n\n");
-        questao6();
-    } else if(num == 7) {
-        printf("\n\nquestao 7\n\n");
-        questao7();
-    } else {
-        printf("digite somente valores de 2 a 7");
+    while(num >= 2 && num <= 7) {
+        if(num == 2) {
+            printf("questao 2\n\n");
+            questao2();
+
+            printf("\n\ndigite qualquer numero diferente de 2 a 7 para parar\n\n");
+            scanf("%d", &num);
+
+        } else if(num == 3) {
+            printf("\n\nquestao 3\n\n");
+            questao3();
+
+            printf("\n\ndigite qualquer numero diferente de 2 a 7 para parar\n\n");
+            scanf("%d", &num);
+
+        } else if(num == 4) {
+            printf("\n\nquestao 4\n\n");
+            questao4();
+
+            printf("\n\ndigite qualquer numero diferente de 2 a 7 para parar\n\n");
+            scanf("%d", &num);
+
+        } else if(num == 5) {
+            printf("\n\nquestao 5\n\n");
+            questao5();
+
+        } else if(num == 6) {
+            printf("\n\nquestao 6\n\n");
+            questao6();
+
+            printf("\n\ndigite qualquer numero diferente de 2 a 7 para parar\n\n");
+            scanf("%d", &num);
+
+        } else if(num == 7) {
+            printf("\n\nquestao 7\n\n");
+            questao7();
+
+            printf("\n\ndigite qualquer numero diferente de 2 a 7 para parar\n\n");
+            scanf("%d", &num);
+
+        }
     }
+    printf("\n\nfinalizando.........\n\n");
 
 
     return 0;
